67cdb97cb8210c044b4e8a6f9db291bb406f67b5
TwoThousand07 <aytimov.erzhanae@gmail.com>
2026-04-07 20:17:52 +0500

    feat: add models in User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+
+    is_active = models.BooleanField(default=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.email
