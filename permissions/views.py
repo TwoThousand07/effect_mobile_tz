bc05364ff223dd939069aabfb25808db736b3910
TwoThousand07 <aytimov.erzhanae@gmail.com>
2026-04-09 21:54:42 +0500

    feat: update permission view in permissions

--- permissions/views.py
+++ permissions/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .services import has_permission
+from .models import Permission
+
+@api_view(["POST"])
+def update_permission(request):
+    if not request.user:
+        return Response({"error": "Unauthoraized"}, status=401)
+    
+    if request.user.role.name != "admin":
+        return Response(status=403)
+    
+    data = request.data
+    
+    perm = Permission.objects.get(id=data["id"])
+    
+    perm.can_read = data.get("can_read", perm.can_read)
+    perm.can_create = data.get("can_create", perm.can_create)
+    perm.can_update = data.get("can_update", perm.can_update)
+    perm.can_delete = data.get("can_delete", perm.can_delete)
+    
+    perm.save()
+    
+    return Response({"message": "updated"})
