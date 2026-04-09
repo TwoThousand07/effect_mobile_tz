648ee6e1312fe2a890ef9a0f221d5073fc88f701
TwoThousand07 <aytimov.erzhanae@gmail.com>
2026-04-09 21:48:17 +0500

    feat: views in api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from permissions.services import has_permission
+
+
+@api_view(["GET"])
+def products(request):
+    if not request.user:
+        return Response({"error": "Unauthoraized"}, status=401)
+
+    if not has_permission(request.user, "products", "read"):
+        return Response({"error": "Forbidden"}, status=403)
+
+    return Response(["item1", "item2"])
+
+
+@api_view(["POST"])
+def create_products(request):
+    if not request.user:
+        return Response({"error": "Unauthoraized"}, status=401)
+
+    if not has_permission(request.user, "products", "read"):
+        return Response({"error": "Forbidden"}, status=403)
+    
+    return Response({"message": "created"})
---
+++ permissions/services.py
@@ -0,0 +1,15 @@
+from .models import Permission
+
+def has_permission(user, resource, action):
+    if not user:
+        return False
+    
+    if not user.role:
+        return False
+    
+    permission = Permission.objects.filter(role=user.role, resource=resource).first()
+    
+    if not permission:
+        return False
+    
+    return getattr(permission, f"can {action}", False)
